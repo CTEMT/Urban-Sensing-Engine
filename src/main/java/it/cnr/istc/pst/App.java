a1b944f0220fe1de49eff6817602d48f6c8ab2d3
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2022-01-17 10:31:20 +0100

    starting major refactoring.. switching to java..

---
+++ src/main/java/it/cnr/istc/pst/App.java
@@ -0,0 +1,16 @@
+package it.cnr.istc.pst;
+
+import java.util.logging.Logger;
+
+/**
+ * Urban-Sensing-Enging
+ *
+ */
+public class App {
+
+    private static Logger LOG = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) {
+        LOG.info("Starting Urban Sensing Engine..");
+    }
+}
