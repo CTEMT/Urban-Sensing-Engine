bbe45f89f2c9582a4c83bcad72d5531f73d13f3b
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2022-01-29 10:30:36 +0100

    added average effect..

--- src/main/java/it/cnr/istc/pst/use/Effect.java
+++ src/main/java/it/cnr/istc/pst/use/Effect.java
@@ -7,14 +7,17 @@ import java.util.logging.Logger;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes({ @Type(value = Effect.AndEffect.class, name = "and"),
         @Type(value = Effect.TextEffect.class, name = "text"),
         @Type(value = Effect.MQTTEffect.class, name = "mqtt"),
-        @Type(value = Effect.IncreaseEffect.class, name = "increase") })
+        @Type(value = Effect.IncreaseEffect.class, name = "increase"),
+        @Type(value = Effect.AverageEffect.class, name = "average") })
 public abstract class Effect {
 
     public abstract void apply(final UrbanSensingEngine use);
@@ -101,8 +104,48 @@ public abstract class Effect {
             try {
                 if (use.get(field) == null)
                     use.set(field, UrbanSensingEngine.OBJECT_MAPPER.readTree(Double.toString(0)));
-                use.set(field, UrbanSensingEngine.OBJECT_MAPPER
-                        .readTree(Double.toString(use.get(field).asDouble() + amount)));
+                else
+                    use.set(field, UrbanSensingEngine.OBJECT_MAPPER
+                            .readTree(Double.toString(use.get(field).asDouble() + amount)));
+            } catch (final JsonProcessingException e) {
+                LOG.log(Level.SEVERE, "Parsing failure..", e);
+            }
+        }
+    }
+
+    public static class AverageEffect extends Effect {
+
+        private static Logger LOG = Logger.getLogger(AverageEffect.class.getName());
+        private final String field;
+        private final String average_field;
+        private final int buffer_size;
+
+        public AverageEffect(@JsonProperty("field") final String field,
+                @JsonProperty("average_field") final String average_field,
+                @JsonProperty("buffer_size") final int buffer_size) {
+            this.field = field;
+            this.average_field = average_field;
+            this.buffer_size = buffer_size;
+        }
+
+        @Override
+        public void apply(UrbanSensingEngine use) {
+            try {
+                if (use.get(average_field) == null)
+                    use.set(average_field, UrbanSensingEngine.OBJECT_MAPPER.readTree("{\"buffer\":[], \"average\":0}"));
+                else {
+                    var av_node = use.get(average_field).deepCopy();
+                    var buffer = av_node.get("buffer");
+                    if (buffer.size() == buffer_size)
+                        ((ArrayNode) buffer).remove(0);
+                    ((ArrayNode) buffer).add(use.get(field));
+                    double avg = 0;
+                    for (var n : buffer)
+                        avg += n.asDouble();
+                    avg /= buffer.size();
+                    ((ObjectNode) av_node).replace("average", UrbanSensingEngine.OBJECT_MAPPER
+                            .readTree(Double.toString(avg)));
+                }
             } catch (final JsonProcessingException e) {
                 LOG.log(Level.SEVERE, "Parsing failure..", e);
             }
