9a1a07bb46dd3ef3c4c30af580d4fea599ef18d8
riccardodebenedictis <riccardo.debenedictis@istc.cnr.it>
2023-01-03 12:23:22 +0100

    minor improvements..

--- include/urban_sensing_engine.h
+++ include/urban_sensing_engine.h
@@ -2,13 +2,14 @@
 
 #include "mongo_db.h"
 #include "coco.h"
+#include "mqtt_middleware.h"
 
 namespace use
 {
   class urban_sensing_engine
   {
   public:
-    urban_sensing_engine();
+    urban_sensing_engine(const std::string &root = COCO_ROOT, const std::string &mongodb_uri = MONGODB_URI(MONGODB_HOST, MONGODB_PORT), const std::string &mqtt_uri = MQTT_URI(MQTT_HOST, MQTT_PORT));
     ~urban_sensing_engine();
 
   private:
--- src/urban_sensing_engine.cpp
+++ src/urban_sensing_engine.cpp
@@ -2,6 +2,10 @@
 
 namespace use
 {
-    urban_sensing_engine::urban_sensing_engine() : coco(db) {}
+    urban_sensing_engine::urban_sensing_engine(const std::string &root, const std::string &mongodb_uri, const std::string &mqtt_uri) : db(root, mongodb_uri), coco(db)
+    {
+        auto mqtt = std::make_unique<coco::mqtt_middleware>(coco);
+        coco.add_middleware(std::move(mqtt));
+    }
     urban_sensing_engine::~urban_sensing_engine() {}
 } // namespace use
