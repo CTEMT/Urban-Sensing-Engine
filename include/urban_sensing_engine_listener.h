afdf2cea9714a53ab806be49efef7b1d2c844d79
riccardodebenedictis <riccardo.debenedictis@istc.cnr.it>
2023-01-16 16:30:39 +0100

    minor cleanings..

--- include/urban_sensing_engine_listener.h
+++ include/urban_sensing_engine_listener.h
@@ -7,11 +7,14 @@ namespace use
 {
   class urban_sensing_engine_listener : public coco::coco_listener
   {
+    friend class urban_sensing_engine;
+
   public:
     urban_sensing_engine_listener(urban_sensing_engine &use) : coco_listener(use), use(use) { use.listeners.push_back(this); }
     urban_sensing_engine_listener(const urban_sensing_engine_listener &orig) = delete;
     virtual ~urban_sensing_engine_listener() { use.listeners.erase(std::find(use.listeners.cbegin(), use.listeners.cend(), this)); }
 
+  private:
     virtual void new_message([[maybe_unused]] const std::string &level, [[maybe_unused]] const std::string &content) {}
     virtual void new_map_message([[maybe_unused]] const std::string &level, [[maybe_unused]] const double &lat, [[maybe_unused]] const double &lng, [[maybe_unused]] const std::string &content) {}
     virtual void new_bus_data([[maybe_unused]] const std::string &bus_id, [[maybe_unused]] const long &time, [[maybe_unused]] const double &lat, [[maybe_unused]] const double &lng, [[maybe_unused]] const long &passengers) {}
