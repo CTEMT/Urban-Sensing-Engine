d7c546347de70dfce123e82ca64318d6711658ee
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-11-26 11:30:46 +0100

    feat: Refactor USPE configuration to use dynamic JSON item loading and add default items

--- src/main.cpp
+++ src/main.cpp
@@ -22,19 +22,6 @@ int main()
     LOG_DEBUG("Loading USPE configuration");
     coco::config(cc);
 
-    auto itms = cc.get_items();
-    if (itms.empty())
-    {
-        LOG_WARN("No items found in the database. Creating default items.");
-        auto &loc_tp = cc.get_type("Location");
-        auto &rock_church_tp = cc.get_type("RockChurch");
-
-        auto &loc0 = cc.create_item({loc_tp}, {{"lat", 40.662238}, {"lng", 16.611671}, {"name", "San Leonardo"}, {"address", "Sasso Caveoso, Matera, Italy"}});
-        [[maybe_unused]] auto &rc0 = cc.create_item({rock_church_tp}, {{"name", "Chiesa di San Leonardo"}, {"location", loc0.get_id()}});
-
-        LOG_INFO("Default items created.");
-    }
-
     LOG_INFO("Starting USPE server...");
     coco::coco_server srv(cc);
     LOG_DEBUG("Adding USPE server module");
