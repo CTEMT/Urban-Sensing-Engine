b85dd870715f2e70bc4d54b7bfe17c6f85e3d8e3
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-12-06 19:47:05 +0100

    feat: Add TemperatureComponent with gauge chart functionality and integrate into USPE app

--- gui/src/main.ts
+++ gui/src/main.ts
@@ -1,6 +1,7 @@
 import { AppComponent, BrandComponent, Connection, Settings } from '@ratiosolver/flick';
 import { coco } from '@ratiosolver/coco';
 import { Offcanvas as USPEOffcanvas } from './offcanvas';
+import { TemperatureComponent } from './temperature';
 import './style.css'
 import { Offcanvas } from 'bootstrap';
 
@@ -11,15 +12,20 @@ const offcanvas_id = 'restart-offcanvas';
 class USPEBrandComponent extends BrandComponent {
 
   constructor() {
-    super('Urban Sensing & Planning Engine', 'logo.png', 180, 60);
+    super('Urban Sensing & Planning Engine', 'logo.png', 120, 40);
     this.node.id = 'uspe-brand';
   }
 }
 
 class USPEApp extends AppComponent {
 
+  private temperature_component: TemperatureComponent;
+
   constructor() {
     super(new USPEBrandComponent());
+
+    this.temperature_component = new TemperatureComponent();
+    this.add_child(this.temperature_component);
 
     const offcanvas = new USPEOffcanvas(offcanvas_id);
     this.add_child(offcanvas);
---
+++ gui/src/temperature.ts
@@ -0,0 +1,163 @@
+import { Component } from "@ratiosolver/flick";
+import * as echarts from 'echarts';
+import type { ECharts, EChartsOption } from 'echarts';
+
+export class TemperatureComponent extends Component<HTMLDivElement> {
+
+  private gaugeChart?: ECharts;
+  private temperature: number = 20;
+
+  constructor() {
+    super(document.createElement('div'));
+    this.node.id = 'temperature-component';
+    Object.assign(this.node.style, { width: '350px', height: '300px', margin: '1rem auto' });
+
+    this.initializeChartWhenReady();
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  public set_temperature(value: number): void {
+    this.temperature = value;
+    this.updateGauge();
+  }
+
+  private initializeChartWhenReady(): void {
+    if (this.node.isConnected && this.node.clientWidth > 0 && this.node.clientHeight > 0) {
+      this.createGauge();
+      return;
+    }
+
+    requestAnimationFrame(() => this.initializeChartWhenReady());
+  }
+
+  private createGauge(): void {
+    if (this.gaugeChart) {
+      return;
+    }
+
+    this.gaugeChart = echarts.init(this.node);
+    this.updateGauge();
+  }
+
+  private updateGauge(): void {
+    if (!this.gaugeChart) {
+      return;
+    }
+
+    this.gaugeChart.setOption(this.buildOption());
+  }
+
+  private buildOption(): EChartsOption {
+    return {
+      series: [{
+        type: 'gauge',
+        center: ['50%', '60%'],
+        startAngle: 200,
+        endAngle: -20,
+        min: -10,
+        max: 50,
+        splitNumber: 12,
+        itemStyle: {
+          color: '#FFAB91'
+        },
+        progress: {
+          show: true,
+          width: 30
+        },
+        pointer: {
+          show: false
+        },
+        axisLine: {
+          lineStyle: {
+            width: 30
+          }
+        },
+        axisTick: {
+          distance: -45,
+          splitNumber: 5,
+          lineStyle: {
+            width: 2,
+            color: '#999'
+          }
+        },
+        splitLine: {
+          distance: -52,
+          length: 14,
+          lineStyle: {
+            width: 3,
+            color: '#999'
+          }
+        },
+        axisLabel: {
+          distance: -20,
+          color: '#999',
+          fontSize: 20
+        },
+        anchor: {
+          show: false
+        },
+        title: {
+          show: false
+        },
+        detail: {
+          valueAnimation: true,
+          width: '60%',
+          lineHeight: 40,
+          borderRadius: 8,
+          offsetCenter: [0, '-15%'],
+          fontSize: 30,
+          fontWeight: 'bolder',
+          formatter: '{value} °C',
+          color: 'inherit'
+        },
+        data: [
+          {
+            value: this.temperature
+          }
+        ]
+      },
+      {
+        type: 'gauge',
+        center: ['50%', '60%'],
+        startAngle: 200,
+        endAngle: -20,
+        min: -10,
+        max: 50,
+        itemStyle: {
+          color: '#FD7347'
+        },
+        progress: {
+          show: true,
+          width: 8
+        },
+        pointer: {
+          show: false
+        },
+        axisLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        },
+        splitLine: {
+          show: false
+        },
+        axisLabel: {
+          show: false
+        },
+        detail: {
+          show: false
+        },
+        data: [
+          {
+            value: this.temperature
+          }
+        ]
+      }]
+    };
+  }
+
+  private handleResize = (): void => {
+    this.gaugeChart?.resize();
+  };
+}
